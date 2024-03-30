12cec97cfc14d4b6daa81b69df1f47ca9eddab52
vinh0212 <vinhqwert2004@gmail.com>
2024-03-30 13:37:43 +0700

    Update LoginController.java

--- flcenter/src/main/java/vn/tad_sebs/Controller/LoginController.java
+++ flcenter/src/main/java/vn/tad_sebs/Controller/LoginController.java
@@ -6,8 +6,34 @@ import java.awt.event.ActionListener;
 import vn.tad_sebs.DAO.UserDao;
 import vn.tad_sebs.Model.User;
 import vn.tad_sebs.View.LoginView;
-import vn.tad_sebs.View.StudentView;
+//import vn.tad_sebs.View.StudentView;
 
 public class LoginController {
-    
+    private LoginView view;
+    private UserDao userDao;
+
+    public LoginController(LoginView view) {
+        this.view = view;
+        userDao = new UserDao();
+        this.view.addLoginListener(new LoginListener());
+    }
+
+    public void showLoginView() {
+        view.showLoginView();
+    }
+
+    class LoginListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            User user = view.getUser();
+            if (userDao.checkUser(user)) {
+                view.showMessage("Đăng nhập thành công");
+                // hiển thị màn hình student
+                //StudentView studentView = new StudentView();
+                //StudentController studentController = new StudentController(studentView);
+                //studentController.showStudentView();
+            } else {
+                view.showMessage("Đăng nhập thất bại");
+            }
+        }
+    }
 }
