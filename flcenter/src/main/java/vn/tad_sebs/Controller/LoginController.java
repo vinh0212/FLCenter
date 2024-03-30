f2583272e07a9a543544b2576209b9da99015cdc
vinh0212 <vinhqwert2004@gmail.com>
2024-03-30 14:03:57 +0700

    Update LoginController.java

--- flcenter/src/main/java/vn/tad_sebs/Controller/LoginController.java
+++ flcenter/src/main/java/vn/tad_sebs/Controller/LoginController.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionListener;
 import vn.tad_sebs.DAO.UserDao;
 import vn.tad_sebs.Model.User;
 import vn.tad_sebs.View.LoginView;
-//import vn.tad_sebs.View.StudentView;
+//import vn.tad_sebs.View.MainView;
 
 public class LoginController {
     private LoginView view;
@@ -27,10 +27,9 @@ public class LoginController {
             User user = view.getUser();
             if (userDao.checkUser(user)) {
                 view.showMessage("Đăng nhập thành công");
-                // hiển thị màn hình student
-                //StudentView studentView = new StudentView();
-                //StudentController studentController = new StudentController(studentView);
-                //studentController.showStudentView();
+                // hiển thị màn hình chính
+                // MainView mainView = new MainView();
+                // mainView.showMainView();
             } else {
                 view.showMessage("Đăng nhập thất bại");
             }
