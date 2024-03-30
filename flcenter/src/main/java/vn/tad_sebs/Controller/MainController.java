49eef3228846825063291fd058d20a96cb9e749e
vinh0212 <vinhqwert2004@gmail.com>
2024-03-30 14:46:39 +0700

    Update MainController.java

--- flcenter/src/main/java/vn/tad_sebs/Controller/MainController.java
+++ flcenter/src/main/java/vn/tad_sebs/Controller/MainController.java
@@ -7,11 +7,14 @@ import vn.tad_sebs.View.LoginView;
 import vn.tad_sebs.View.MainView;
 
 public class MainController {
-    private MainView view;
+    private MainView mainView;
 
-    public MainController(MainView view)
-    {
-        this.view = view;
-        
+    public MainController(MainView mainView) {
+        this.mainView = mainView;
+        this.mainView.addQLHVListener(new QLHVListener());
+        //this.mainView.addQLLHListener(new QLLHListener());
+        //this.mainView.addQLCBGVListener(new QLCBGVListener());
+        //this.mainView.addQLCTHListener(new QLCTHListener());
+        //this.mainView.addQLKTListener(new QLKTListener());
     }
 }
